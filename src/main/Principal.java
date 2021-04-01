04a3b2f037f8e1d37d6a679e8d61e09a69c17fc1
Estiven Dubon <estivendubon123@gmail.com>
2021-03-31 18:16:51 -0600

    agregando etiqueta 1

--- src/main/Principal.java
+++ src/main/Principal.java
@@ -6,11 +6,12 @@
 package main;
 
 import java.awt.Color;
+import java.awt.Font;
 import javax.swing.ImageIcon;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
-import javax.swing.WindowConstants;
+
 
 /**
  *
@@ -24,10 +25,23 @@ public class Principal {
         myFrame.setSize(500,500);
         myFrame.setVisible(true);
         myFrame.setLocationRelativeTo(null);
-        myFrame.setResizable(false);
+        myFrame.setLayout(null);
+       // myFrame.setResizable(false);
         
         JPanel panel=new JPanel();
+        panel.setSize(500,500);
         panel.setBackground(new Color(150,25,170));
+        panel.setLayout(null);
+        
+        JLabel label =new JLabel("hello there");
+        label.setFont(new Font("Comic Sans MS",Font.BOLD,25));
+        label.setForeground(Color.WHITE);
+        label.setBounds(170,50,300,60);
+        
+        //ImageIcon image=new ImageIcon("like.png");
+        
+         //ImageIcon imagen =new ImageIcon("like.png");
+       panel.add(label);
         
         
         myFrame.add(panel);
